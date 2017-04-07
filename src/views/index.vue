5a89353f44c0947a83ea980fcf73be96b4505737
tutuzimu <18702601682@163.com>
2017-04-07 09:04:22 +0800

    1234567 Signed-off-by: tutuzimu <18702601682@163.com>

---
+++ src/views/index.vue
@@ -0,0 +1,140 @@
+
+<template>
+    <div class="layout">
+        <Menu mode="horizontal" theme="dark" active-name="1">
+            <div class="layout-logo"></div>
+            <div class="layout-nav">
+                <Menu-item name="1">
+                <Icon type="ios-navigate"></Icon>
+                    导航一
+                </Menu-item>
+                <Menu-item name="2">
+                    <Icon type="ios-keypad"></Icon>
+                    导航二
+                </Menu-item>
+                <Menu-item name="3">
+                    <Icon type="ios-analytics"></Icon>
+                    导航三
+                </Menu-item>
+                <Menu-item name="4">
+                    <Icon type="ios-paper"></Icon>
+                    导航四
+                </Menu-item>
+            </div>
+        </Menu>
+        <Menu mode="horizontal" active-name="1">
+            <div class="layout-assistant">
+                <Menu-item name="1">二级导航</Menu-item>
+                <Menu-item name="2">二级导航</Menu-item>
+                <Menu-item name="3">二级导航</Menu-item>
+            </div>
+        </Menu>
+        <div class="layout-breadcrumb">
+            <Breadcrumb>
+                <Breadcrumb-item href="#">首页</Breadcrumb-item>
+                <Breadcrumb-item href="#">应用中心</Breadcrumb-item>
+                <Breadcrumb-item>某应用</Breadcrumb-item>
+            </Breadcrumb>
+        </div>
+        <div class="layout-content">
+            内容区域
+        </div>
+        <div class="layout-copy">
+            2011-2016 &copy; TalkingData
+        </div>
+    </div>
+</template>
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav{
+        width: 420px;
+        margin: 0 auto;
+    }
+    .layout-assistant{
+        width: 300px;
+        margin: 0 auto;
+        height: inherit;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 200px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+</style>
+<script>
+    export default {
+        components: {
+
+        },
+        data () {
+            return {
+                 data1: this.getMockData(),
+                targetKeys1: this.getTargetKeys()
+            }
+        },
+        methods: {
+            bindUser(){
+
+            },
+            register(){
+
+            },
+             getMockData () {
+                let mockData = [];
+                for (let i = 1; i <= 20; i++) {
+                    mockData.push({
+                        key: i.toString(),
+                        label: '内容' + i,
+                        description: '内容' + i + '的描述信息',
+                        disabled: Math.random() * 3 < 1
+                    });
+                }
+                return mockData;
+            },
+            getTargetKeys () {
+                return this.getMockData()
+                        .filter(() => Math.random() * 2 > 1)
+                        .map(item => item.key);
+            },
+            render1 (item) {
+                return item.label;
+            },
+            handleChange1 (newTargetKeys, direction, moveKeys) {
+                console.log(newTargetKeys);
+                console.log(direction);
+                console.log(moveKeys);
+                this.targetKeys1 = newTargetKeys;
+            }
+            
+        },
+        ready () {
+
+        }
+    }
+</script>
